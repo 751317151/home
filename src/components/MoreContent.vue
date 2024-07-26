eadb429bd5ae3bbe390dbbf79adbbcf0fcefd1e1
华豪 <huah@sunwayworld.com>
2024-07-26 16:38:52 +0800

    【PM20220320002】【20117258】 代码规范修改，重构 【下班倒计时】

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,6 +1,55 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">
+    <div class="time-title" v-if="!common.isEmpty(state.time.countdownTime)">
+      {{ state.countdownChange }}
+    </div>
+  </div>
 </template>
+
+<script setup>
+import common from "@/utils/common";
+import { reactive } from "vue";
+
+const state = reactive({
+  time: {
+    countdownTime: "2099-9-26 17:30:00",
+  },
+  countdownChange: "",
+});
+
+const countdown = () => {
+  if (common.isEmpty(state.time.countdownTime)) {
+    return;
+  }
+  let now = new Date();
+  if (xiuxi(now)) {
+    state.countdownChange = "别卷了，好好休息吧~";
+  } else {
+    let countdown = common.countdown(state.time.countdownTime, now);
+    state.countdownChange =
+      "下班倒计时: " + countdown.h + "时" + countdown.m + "分" + countdown.s + "秒";
+  }
+};
+const xiuxi = (now) => {
+  let weekDay = now.getDay();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  if (weekDay == 0 || weekDay == 6) {
+    return true;
+  }
+  if (hour > 17 || (hour == 17 && minute >= 30)) {
+    return true;
+  }
+  if (hour < 8 || (hour == 8 && minute < 30)) {
+    return true;
+  }
+  return false;
+};
+
+setInterval(() => {
+  countdown();
+}, 1000);
+</script>
 
 <style lang="scss" scoped>
 .more-content {
@@ -10,5 +59,12 @@
   margin-top: 20px;
   width: 100%;
   height: 100%;
+}
+.time-title {
+  text-align: center;
+  font-size: 1.5rem;
+  line-height: 4rem;
+  font-weight: 600;
+  letter-spacing: 2px;
 }
 </style>
